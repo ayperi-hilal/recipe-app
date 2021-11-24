948e3e9772373a06070a703c7854e602bed5c97d
Maide Yıldız <maideyildizz@gmail.com>
2021-11-24 17:30:34 +0300

    login page

---
+++ constants/images.js
@@ -0,0 +1,7 @@
+const loginBackground = require("../assets/landing-pizza.jpg")
+
+
+
+export default {
+    loginBackground,
+}
--- screens/Landing.js
+++ screens/Landing.js
@@ -7,12 +7,76 @@ import {
     ImageBackground,
     StatusBar
  } from 'react-native';
- import {COLORS,SIZES,FONTS} from "../constants"
+ import {images,COLORS,SIZES,FONTS} from '../constants';
+ import { LinearGradient } from 'expo-linear-gradient';
 
 function Landing({navigation}) {
+    function renderHeader(){
+        return(
+            <View 
+            style={{
+                height: SIZES.height > 700 ? "65%" : "60%"
+            }}>
+                <ImageBackground 
+                source={images.loginBackground}
+                style={{
+                    flex:1,
+                    justifyContent:'flex-end'
+                }}
+                resizeMode="cover">
+                    <LinearGradient
+                    start={{ x:0,y:0 } }
+                    end={{ x:0,y:1 }}
+                    colors={[
+                        COLORS.transparent,
+                        COLORS.black
+                    ]}
+                    style={{
+                        height:200,
+                        justifyContent:'flex-end',
+                        paddingHorizontal:SIZES.padding
+                    }}
+                    >
+                        <Text
+                        style={{
+                            width:"100%",
+                            color:COLORS.white,
+                            ...FONTS.largeTitle,
+                            lineHeight:45
+                        }}>
+                            En güzel tariflerini deneyin!
+                        </Text>
+                    </LinearGradient>
+                </ImageBackground>
+            </View>
+
+        )
+    }
+    function renderDetail(){
+        return(
+            <View 
+            style={{
+                flex:1,
+                paddingHorizontal:SIZES.padding
+            }}>
+                <Text
+                style={{
+                    marginTop:SIZES.radius,
+                    width:"70%",
+                    color:COLORS.gray,
+                    ...FONTS.body3
+                }}>
+                    En güzel tarifleri keşfet ve deneme şansı yakala...
+                </Text>
+            </View>
+
+        )
+    }
     return (
         <View style={styles.login}>
-            <StatusBar barStyle="dark-content"/>
+            <StatusBar barStyle="light-content"/>
+            {renderHeader()}
+            {renderDetail()}
         </View>
     )
 }
@@ -20,6 +84,6 @@ const styles = StyleSheet.create({
     login: {
       flex: 1,
       backgroundColor: '#000',
-    },
+    }
   });
 export default Landing
