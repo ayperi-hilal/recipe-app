61af7a1edbd06e7265f0128df45e4c72e82c9695
Maide Yıldız <maideyildizz@gmail.com>
2021-11-24 15:35:24 +0300

    adding login

--- screens/Landing.js
+++ screens/Landing.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+    StyleSheet,
+    Text, 
+    View,
+    TouchableOpacity,
+    ImageBackground,
+    StatusBar
+ } from 'react-native';
+ import {COLORS,SIZES,FONTS} from "../constants"
 
-function Landing() {
+function Landing({navigation}) {
     return (
-        <div>
-            
-        </div>
+        <View style={styles.login}>
+            <StatusBar barStyle="dark-content"/>
+        </View>
     )
 }
-
+const styles = StyleSheet.create({
+    login: {
+      flex: 1,
+      backgroundColor: '#000',
+    },
+  });
 export default Landing
